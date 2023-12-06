c7a02ad8358fc9f780b32f69b5ce074c03ce5e67
Kamal Yeshodhar Shastry Gattu <gkyshastry0502@gmail.com>
2023-12-05 22:45:09 -0500

    Skelton Code Completed

--- BART-Base.py
+++ BART-Base.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
+import re
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
 
 import torch
 from torch.nn.utils.rnn import pad_sequence
@@ -11,11 +17,82 @@ import pandas as pd
 from rouge_score import rouge_scorer
 from torch.cuda.amp import autocast, GradScaler
 from torch.nn.utils import clip_grad_norm_
+from contractions import contractions_dict
+def expand_contractions(text, contraction_map=None):
+    if contraction_map is None:
+        contraction_map = contractions_dict
+
+    # Using regex for getting all contracted words
+    contractions_keys = '|'.join(re.escape(key) for key in contraction_map.keys())
+    contractions_pattern = re.compile(f'({contractions_keys})', flags=re.DOTALL)
+
+    expanded_text = contractions_pattern.sub(lambda match: contraction_map.get(match.group(0), match.group(0)), text)
+    expanded_text = re.sub("'", "", expanded_text)
+    return expanded_text
+entertainment = pd.read_csv('data/BBCNewsSummaryCSV/entertainment_data.csv')
+sport = pd.read_csv('data/BBCNewsSummaryCSV/sport_data.csv')
+tech = pd.read_csv('data/BBCNewsSummaryCSV/tech_data.csv')
+business = pd.read_csv('data/BBCNewsSummaryCSV/business_data.csv')
+politics = pd.read_csv('data/BBCNewsSummaryCSV/politics_data.csv')
+training_dataset = pd.concat([entertainment,sport,tech], ignore_index=True)
+testing_dataset = pd.concat([business,politics], ignore_index=True)
+print("Training size:",training_dataset.size)
+print("Testing size:",testing_dataset.size)
+training_dataset = training_dataset.sample(frac=1).reset_index(drop=True)
+testing_dataset = testing_dataset.sample(frac=1).reset_index(drop=True)
+training_dataset['newsarticle'] = training_dataset['newsarticle'].apply(expand_contractions)
+testing_dataset['newsarticle'] = testing_dataset['newsarticle'].apply(expand_contractions)
+def getSenLen(sentence):
+    return len(sentence.split())
+training_dataset['article_length'] = training_dataset['newsarticle'].apply(getSenLen)
+training_dataset['summary_length'] = training_dataset['summary'].apply(getSenLen)
+testing_dataset['article_length'] = testing_dataset['newsarticle'].apply(getSenLen)
+testing_dataset['summary_length'] = testing_dataset['summary'].apply(getSenLen)
+training_dataset.head()
+training_dataset.describe()
+# Create a subplot with 1 row and 2 columns
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12, 6))
+# Plot for the articles' number of words
+sns.boxplot(training_dataset["article_length"], ax=axes[0])
+axes[0].set_ylabel("Number of Words")
+axes[0].set_title("Boxplot of Article Lengths")
+# Plot for the summaries' number of words
+sns.boxplot(training_dataset["summary_length"], ax=axes[1])
+axes[1].set_ylabel("Number of Words")
+axes[1].set_title("Boxplot of Summary Lengths")
+# Get statistics for the articles boxplot
+lines_articles = axes[0].lines[:6]
+articles_stats = [line.get_ydata()[0] for line in lines_articles]
+Q1_articles, Q3_articles, lower_whisker_articles, upper_whisker_articles, median_articles = articles_stats[:5]
+# Get statistics for the summaries boxplot
+lines_summaries = axes[1].lines[:6]
+summaries_stats = [line.get_ydata()[0] for line in lines_summaries]
+Q1_summaries, Q3_summaries, lower_whisker_summaries, upper_whisker_summaries, median_summaries = summaries_stats[:5]
+print(upper_whisker_articles)
+print(upper_whisker_summaries)
+training_dataset = training_dataset[(training_dataset['summary_length'] <= upper_whisker_summaries) & (training_dataset['article_length'] <= upper_whisker_articles)]
+testing_dataset = testing_dataset[(testing_dataset['summary_length'] <= upper_whisker_summaries) & (testing_dataset['article_length'] <= upper_whisker_articles)]
+training_dataset.head()
+training_dataset.describe()
+df = training_dataset[0:100]
+
+
+# In[20]:
 
 
 # Define the device for GPU usage (if available)
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+if torch.backends.mps.is_available():
+    arch = "mps"
+elif torch.cuda.is_available():
+    arch = "cuda"
+else:
+    arch = "cpu"
+
+device = torch.device(arch)
+
+
+# In[21]:
+
 
 # Tokenize and preprocess the text data
 tokenizer = BartTokenizer.from_pretrained('facebook/bart-base')
@@ -29,141 +106,104 @@ def tokenize_summary(text):
     inputs = tokenizer.encode(text, return_tensors="pt", max_length=280, truncation=True, padding='max_length', return_attention_mask=True)
     return inputs.to(device)  # Move the tokenized summaries to the GPU
 
+def tokenize_and_stack(df, text_column, summary_column):
+    df['TokenizedText'] = df[text_column].apply(tokenize_text)
+    df['TokenizedSummary'] = df[summary_column].apply(tokenize_summary)
+    
+    # Convert tokenized data to PyTorch tensors
+    X = torch.stack([seq.squeeze() for seq in df['TokenizedText']])
+    Y = torch.stack([seq.squeeze() for seq in df['TokenizedSummary']])
+    
+    # Define a DataLoader for batching data
+    dataset = TensorDataset(X, Y)
+    dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
+    
+    return X, Y, dataloader
 
-df['TokenizedText'] = df['newsarticle'].apply(tokenize_text)
-df['TokenizedSummary'] = df['summary'].apply(tokenize_summary)
+train_df, val_df = train_test_split(df, test_size=0.2, random_state=42)
+test_df = testing_dataset[0:len(val_df)]
 
-# Split your data into train and test sets
-train_df, test_df = train_test_split(df, test_size=0.2, random_state=42)
+# Tokenize and stack for training set
+X_train, Y_train, train_dataloader = tokenize_and_stack(train_df, 'newsarticle', 'summary')
 
-# Convert tokenized data to PyTorch tensors
-X_train = torch.stack([seq.squeeze() for seq in train_df['TokenizedText']])
-Y_train = torch.stack([seq.squeeze() for seq in train_df['TokenizedSummary']])
-X_test = torch.stack([seq.squeeze() for seq in test_df['TokenizedText']])
-Y_test = torch.stack([seq.squeeze() for seq in test_df['TokenizedSummary']])
+# Tokenize and stack for validation set
+X_val, Y_val, val_dataloader = tokenize_and_stack(val_df, 'newsarticle', 'summary')
 
-# Define a DataLoader for batching data
-train_dataset = TensorDataset(X_train, Y_train)
-train_dataloader = DataLoader(train_dataset, batch_size=4, shuffle=True)
-test_dataset = TensorDataset(X_test, Y_test)
-test_dataloader = DataLoader(test_dataset, batch_size=4)
+# Tokenize and stack for validation set
+X_test, Y_test, test_dataloader = tokenize_and_stack(test_df, 'newsarticle', 'summary')
+print(X_val.size)
 # Define the BART model
 model = BartForConditionalGeneration.from_pretrained('facebook/bart-base')
-
 # Create a GradScaler for mixed-precision training
 scaler = GradScaler()
-
 # Define hyperparameters
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
 model.to(device)  # Move the model to the GPU
-optimizer = AdamW(model.parameters(), lr=2e-5, weight_decay=0.01)  
+optimizer = torch.optim.AdamW(model.parameters(), lr=2e-5, weight_decay=0.01)  
 scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=50, num_training_steps=len(train_dataloader) * 10)  # Add learning rate scheduler
 early_stopping_rounds = 2
 best_rouge_score = -1
 current_round = 0
-
 # Define gradient accumulation steps
 accumulation_steps = 20  # You can adjust this number
-
-def train(model, dataloader, optimizer, scheduler):
+# Training loop
+for epoch in range(2):  # Change the number of epochs as needed
     model.train()
     total_loss = 0.0
     optimizer.zero_grad()
-
-    for step, batch in enumerate(tqdm(dataloader, desc="Training")):
+    for step, batch in enumerate(tqdm(train_dataloader, desc=f"Epoch {epoch+1}/{2}")):
         inputs = batch[0].to(device)  # Move the input batch to the GPU
         attention_mask = (inputs != 0).float().to(device)  # Create attention mask
         targets = batch[1].to(device)  # Move the target batch to the GPU
-
         with autocast():
             outputs = model(input_ids=inputs, attention_mask=attention_mask, decoder_input_ids=targets, labels=targets)
             loss = outputs.loss
-
         # Perform gradient accumulation
         loss = loss / accumulation_steps
         scaler.scale(loss).backward()
-
         if (step + 1) % accumulation_steps == 0:
             # Update gradients and optimizer once every accumulation_steps
             clip_grad_norm_(model.parameters(), max_norm=1.0)  # Optional gradient clipping
             scaler.step(optimizer)
             scaler.update()
             optimizer.zero_grad()
-
         total_loss += loss.item()
-
-    return total_loss / len(dataloader)
-
-def calculate_rouge1_precision(logits, targets):
-    scorer = rouge_scorer.RougeScorer(['rouge1'], use_stemmer=True)
-    rouge1_precision = 0.0
-    num_samples = len(logits)
-
-    for i in range(num_samples):
-        predicted_ids = logits[i].cpu().numpy()
-        target_ids = targets[i].cpu().numpy()
-
-        # Convert token IDs to strings
-        predicted_text = tokenizer.decode(predicted_ids, skip_special_tokens=True)
-        target_text = tokenizer.decode(target_ids, skip_special_tokens=True)
-
-        # Calculate ROUGE-1 precision
-        scores = scorer.score(predicted_text, target_text)
-        rouge1_precision += scores['rouge1'].precision
-
-    return rouge1_precision / num_samples
-
-# Training loop
-for epoch in range(2):  # Change the number of epochs as needed
-    train_loss = train(model, train_dataloader, optimizer, scheduler)
+    train_loss = total_loss / len(train_dataloader)
     print(f"Epoch {epoch+1}/{2}, Train Loss: {train_loss:.4f}")
-
+# Save the model after training
+model.save_pretrained("saved_model")
 def evaluate(model, dataloader):
     model.eval()
-    
     test_articles = []
     actual_summaries = []
     predicted_summaries = []
     rouge1_precision_scores = []
-
     scorer = rouge_scorer.RougeScorer(['rouge1'])
-
     with torch.no_grad():
         for batch in tqdm(dataloader, desc="Evaluating Test"):
             inputs = batch[0].to(device)
             attention_mask = (inputs != 0).float().to(device)
             targets = batch[1].to(device)
             outputs = model.generate(input_ids=inputs, attention_mask=attention_mask, max_length=150, num_beams=17, length_penalty=2.0, early_stopping=False)
-            
             for output, target, input_text in zip(outputs, targets, inputs):
                 # Calculate ROUGE-1 precision for each sample
                 output_text = tokenizer.decode(output, skip_special_tokens=True)
                 target_text = tokenizer.decode(target, skip_special_tokens=True)
                 rouge_scores = scorer.score(output_text, target_text)
                 rouge1_precision_scores.append(rouge_scores['rouge1'].precision)
-                
                 # Append tokenized text, actual summaries, and predicted summaries
                 test_articles.append(tokenizer.decode(input_text, skip_special_tokens=True))
                 actual_summaries.append(target_text)
                 predicted_summaries.append(output_text)
-
-    return test_articles, actual_summaries, predicted_summaries, rouge1_precision_scores
-
-# Evaluate the model
-test_articles, actual_summaries, predicted_summaries, rouge1_precision_scores = evaluate(model, test_dataloader)
-
-# Create a dictionary with the extracted data
-data = {
+    data = {
     'Article': test_articles,
     'Actual Summary': actual_summaries,
     'Predicted Summary': predicted_summaries,
     'ROUGE-1 Precision': rouge1_precision_scores,
-}
-
-# Create a Pandas DataFrame from the dictionary
-results_df = pd.DataFrame(data)
-
-# Display the DataFrame
-pd.set_option('display.max_colwidth', None)
-results_df.head(5)
+    }
+    results_df = pd.DataFrame(data)
+    return results_df
+    # return test_articles, actual_summaries, predicted_summaries, rouge1_precision_scores
+sameCategoryData = evaluate(model, val_dataloader)
+diffCategoryData = evaluate(model,test_dataloader)
+print(sameCategoryData['ROUGE-1 Precision'])
+print(diffCategoryData['ROUGE-1 Precision'])
